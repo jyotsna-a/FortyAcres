dec42688967b493d4b986a6969d5e059b0a8077e
jyotsna-a <jyotsna.arunkumar109@gmail.com>
2024-02-03 12:34:40 -0600

    Initial Commit

---
+++ FortyACres/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FortyACres
+//
+//  Created by Jyotsna Arunkumar on 2/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
