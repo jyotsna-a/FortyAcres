3b82c88c81af36216e1131abe216a1ea12cdbff2
jyotsna-a <jyotsna.arunkumar109@gmail.com>
2024-02-03 15:55:00 -0600

    "More done"

--- FortyACres/StudentFoodPage.swift
+++ FortyACres/StudentFoodPage.swift
@@ -7,6 +7,55 @@
 
 import UIKit
 
-class StudentFoodPage: UIViewController {
+class Vendor {
+    var name: String
+    var menu: [String]
     
+    init(name: String, menu: [String]) {
+        self.name = name
+        self.menu = menu
+    }
+}
+
+class StudentFoodPage: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var VendorPicker: UIPickerView!
+    var vendors: [Vendor] = [
+            Vendor(name: "Sabor", menu: ["Tacos", "Bowls", "Burritos"]),
+            Vendor(name: "Chick-Fil-A", menu: ["Sandwiches", "Salads"])
+            
+        ]
+    
+        var selectedVendor: Vendor?
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            VendorPicker.delegate = self
+            VendorPicker.dataSource = self
+            selectedVendor = vendors.first
+        }
+        
+        // MARK: - UIPickerViewDataSource Methods
+        func numberOfComponents(in pickerView: UIPickerView) -> Int {
+            return 1
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            return vendors.count
+        }
+        
+        // MARK: - UIPickerViewDelegate Methods
+        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            return vendors[row].name
+        }
+    
+        func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+            selectedVendor = vendors[row]
+        }
+        
+        @IBAction func MenuButton(_ sender: UIButton) {
+            guard let menu = selectedVendor?.menu else { return }
+                print("Menu of \(selectedVendor?.name ?? "Selected Vendor"): \(menu.joined(separator: ", "))")
+        }
 }
